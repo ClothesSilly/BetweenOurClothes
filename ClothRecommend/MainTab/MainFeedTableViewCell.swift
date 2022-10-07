113204e27fe19752acc60edf75046fbf81b0d0bc
yleer <spsyoudong1@naver.com>
2022-10-07 14:31:52 +0900

    Folder Cleaned

--- ClothRecommend/MainTab/MainFeedTableViewCell.swift
+++ ClothRecommend/MainTab/MainFeedTableViewCell.swift
@@ -8,7 +8,7 @@
 import UIKit
 import SnapKit
 
-class MainFeedTableViewCell: UITableViewCell, SetUpView {
+class MainFeedTableViewCell: UITableViewCell {
     
     static let identifier = "MainFeedTableViewCell"
     
