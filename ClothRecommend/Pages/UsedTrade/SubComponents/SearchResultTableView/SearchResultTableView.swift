6fcf629347f9cc16232ab1166872487928f00b44
Yang <75043852+caumannerman@users.noreply.github.com>
2022-10-31 12:47:51 +0900

    검색 TableView 구현 완료

--- ClothRecommend/Pages/UsedTrade/SubComponents/SearchResultTableView/SearchResultTableView.swift
+++ ClothRecommend/Pages/UsedTrade/SubComponents/SearchResultTableView/SearchResultTableView.swift
@@ -5,4 +5,52 @@
 //  Created by 양준식 on 2022/10/30.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+
+class SearchResultTableView: UITableView {
+    let disposeBag = DisposeBag()
+    
+    let headerView = SortFilterView(
+        frame: CGRect(
+            origin: .zero,
+            size: CGSize(width: UIScreen.main.bounds.width, height: 50)
+        )
+    )
+    //MainViewController -> 이곳 tableView
+    //TableView에 뿌려질 SearchResultCellData Entity Data들을 받아올 것임
+    let cellData = PublishSubject<[SearchResultCellData]>()
+    
+    override init(frame: CGRect, style: UITableView.Style) {
+        super.init(frame: frame, style: style)
+        
+        bind()
+        attribute()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func bind(){
+        cellData
+            .asDriver(onErrorJustReturn: [])
+            .drive(self.rx.items) { tv, row, data in
+                let index = IndexPath(row: row, section: 0)
+                let cell = tv.dequeueReusableCell(withIdentifier: "SearchResultTableViewCell", for: index) as! SearchResultTableViewCell
+                cell.setData(data)
+                return cell
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    private func attribute(){
+        self.backgroundColor = .white
+        self.register(SearchResultTableViewCell.self, forCellReuseIdentifier: "SearchResultTableViewCell")
+        self.separatorStyle = .singleLine
+        self.rowHeight = 100
+        self.tableHeaderView = headerView
+    }
+}
--- ClothRecommend/Pages/UsedTrade/SubComponents/SearchResultTableView/SearchResultTableViewCell.swift
+++ ClothRecommend/Pages/UsedTrade/SubComponents/SearchResultTableView/SearchResultTableViewCell.swift
@@ -9,7 +9,7 @@ import UIKit
 import SnapKit
 import Kingfisher
 
-class BlogListCell: UITableViewCell {
+class SearchResultTableViewCell: UITableViewCell {
     
     let thumbnailImageView = UIImageView()
     let titleLabel = UILabel()
