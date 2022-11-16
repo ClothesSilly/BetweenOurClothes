c31d56ba76624581918f7996d3d543cb205b111f
Yang <75043852+caumannerman@users.noreply.github.com>
2022-11-16 14:49:17 +0900

    댓글 리스트뷰와 셀,헤더 푸터뷰 파일 생성

---
+++ ClothRecommend/Pages/PostDetail/SubComponents/CommentListView/CommentListTableHeaderView.swift
@@ -0,0 +1,8 @@
+//
+//  CommentListTableHeaderView.swift
+//  ClothRecommend
+//
+//  Created by 양준식 on 2022/11/16.
+//
+
+import Foundation
---
+++ ClothRecommend/Pages/PostDetail/SubComponents/CommentListView/CommentListTableView.swift
@@ -0,0 +1,8 @@
+//
+//  CommentListTableView.swift
+//  ClothRecommend
+//
+//  Created by 양준식 on 2022/11/16.
+//
+
+import Foundation
